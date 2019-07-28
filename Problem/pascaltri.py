67baf1d18997f90a6759fcf7c3f294f7fc77a87e
vxda7 <vxda77@gmail.com>
2019-07-28 23:27:48 +0900

    error found

--- Problem/pascaltri.py
+++ Problem/pascaltri.py
@@ -5,6 +5,7 @@ for number in range(numbers):
     gets = int(input())
 
     for get in range(gets):
+
         temp=[]
         if get == 0:
             temp.append(1)
@@ -18,15 +19,20 @@ for number in range(numbers):
                 elif i == get:
                     temp.append(1)
                 else:
-                    temp.append(result[get-1][i-1]+result[get-1][i])
+                    temp.append(result[-1][i-1]+result[-1][i])
+ 
         result.append(temp)
 
 print(result)
 cnt=1
 for res in result:
-    print(f'#{cnt}',end=' ')
+    if len(res) == 1:
+        print(f'#{cnt}')
+        cnt+=1
+
     line=''
     for char in res:
         line+=str(char)+' '
     print(f'{line}')
-    cnt+=1
+
+    
