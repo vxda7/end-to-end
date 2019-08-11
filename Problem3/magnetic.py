d02d30982a720b1df41d46c2a4d3183ebd20a67e
vxda7 <vxda77@gmail.com>
2019-08-11 17:50:36 +0900

    magnetic is done

--- Problem3/magnetic.py
+++ Problem3/magnetic.py
@@ -11,32 +11,34 @@ def rot90(gets):
 
 def check(gets):
     gets = rot90(gets)
-    change=0
+    change=0    # 몇번 바뀌는지 확인하는 것을 세서 교착상태를 확인
     for get in gets:
-        first=False
-        save=2
+        first=False #처음에 나오는 수가 2가 아니면 의미가 없음 테이블 밑으로 떨어지므로
+        save=2  # 왼쪽이 S극 = 2 이고 오른쪽이 N극 = 1
+        temp=0  # 하나의 라인마다 저장할 데이터
         for g in get:
-            if g:
-                if first:
-                    if g != save:
-                        save=g
-                        change+=1
-                elif g == 2:
+            if g:   # 값이 0이면 건너뜀
+                if first:   # 첫값이 2인지 확인 
+                    if g != save:   # 저장되어있는 값과 다르면 temp를 1올려줌
+                        save=g  # 받은 값을 저장
+                        temp+=1 # 임시 change를 1추가
+                elif g == 2:    #첫값이 2이면 first를 True로 바꿔서 시작
                     first=True
-    if change%2:
-        return int((change+1)/2)
-    elif change == 0 :
-        return 0
-    else:
-        return int((change)/2)
+             
+        if temp%2:  # 홀수일 때는 변화량보다 1더한 값의 절반만큼 교착상태
+            change += int((temp+1)/2)
+        elif temp == 0 :
+            change += 0
+        else:   # 짝수일 때는 변화량의 절반만큼 교착상태
+            change += int((temp)/2)
+
+    return change   # 교착상태의 수를 반환
                 
 # 바뀌는 횟수가 짝수면 1빼고 홀수면 1더하기
 
-
-
 #test케이스 횟수
 result=[]
-for num in range(1):
+for num in range(10):
     numbers = int(input())  # 
     # N->1 S->2 up->N down->S
     case=[]
@@ -49,5 +51,5 @@ for num in range(1):
 #출력
 cnt=1
 for res in result:
-    print(f"#{cnt} {res}")
+    print("#{0} {1}".format(cnt, res))
     cnt += 1
